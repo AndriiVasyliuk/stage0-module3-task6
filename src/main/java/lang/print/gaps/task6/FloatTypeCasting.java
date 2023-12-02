1fe9766eb8758bfb398c32a0d2d7e2ff840bfbed
AndriiVasyliuk <AndriiVasyliukPro@gmail.com>
2023-12-02 21:35:48 +0200

    [modify] change files to reach the goal

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task6;
 
+import static java.lang.Math.round;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        System.out.println((int)round(numberToBeRounded));
     }
 }
